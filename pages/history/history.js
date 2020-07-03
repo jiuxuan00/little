05ff6225f1f13951ef110a780d91b75f5384377d
旧轩00°° <117898271@qq.com>
2020-07-03 19:49:43 +0800

    history 完成

---
+++ pages/history/history.js
@@ -0,0 +1,102 @@
+// pages/history/history.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hotList:[
+      {
+        id: 1,
+        imgUrl: 'https://img14.360buyimg.com/n1/jfs/t3682/362/1363475238/559844/ab37eb62/58229915N5796455e.jpg',
+        title: '春夏亲肤棉四件套床单款全棉芦荟棉11',
+        price: 11,
+        payment: 111
+      },
+      {
+        id: 2,
+        imgUrl: 'https://img14.360buyimg.com/n1/jfs/t3682/362/1363475238/559844/ab37eb62/58229915N5796455e.jpg',
+        title: '春夏亲肤棉四件套床单款全棉芦荟棉11',
+        price: 22,
+        payment: 222
+      },
+      {
+        id: 3,
+        imgUrl: 'https://img14.360buyimg.com/n1/jfs/t3682/362/1363475238/559844/ab37eb62/58229915N5796455e.jpg',
+        title: '春夏亲肤棉四件套床单款全棉芦荟棉11',
+        price: 33,
+        payment: 333
+      },
+      {
+        id: 4,
+        imgUrl: 'https://img14.360buyimg.com/n1/jfs/t3682/362/1363475238/559844/ab37eb62/58229915N5796455e.jpg',
+        title: '春夏亲肤棉四件套床单款全棉芦荟棉11',
+        price: 44,
+        payment: 444
+      },
+      {
+        id: 5,
+        imgUrl: 'https://img14.360buyimg.com/n1/jfs/t3682/362/1363475238/559844/ab37eb62/58229915N5796455e.jpg',
+        title: '春夏亲肤棉四件套床单款全棉芦荟棉11',
+        price: 55,
+        payment: 555
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ readme.md
@@ -0,0 +1,12 @@
+"pages/index/index",  //首页
+"pages/goods/index",  // 商品列表
+"pages/cart/index",  // 购物车
+"pages/agent/index", // 成为代理
+"pages/goodsDetail/goodsDetail", //商品详情
+"pages/other/loading/loading",
+"pages/my/index/index", // 个人中心
+"pages/my/edit/edit", // 修改个人资料
+"pages/my/address/address", //选择收货地址
+"pages/my/orderConfirm/orderConfirm", // 确认订单
+"pages/my/orders/orders", //全部订单
+"pages/history/history" // 历史浏览
