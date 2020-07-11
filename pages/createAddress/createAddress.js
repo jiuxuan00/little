36364fa42eb26b26e533527475eaf55be5b69d75
旧轩00°° <117898271@qq.com>
2020-07-11 23:01:35 +0800

    选择城市弹出框完成

--- pages/createAddress/createAddress.js
+++ pages/createAddress/createAddress.js
@@ -1,3 +1,6 @@
+// var areaData = require("./data/area.js");
+import areaData from './data/area'
+
 // pages/createAddress/createAddress.js
 Page({
 
@@ -5,16 +8,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    show: false,
+    areaList: {
+      province_list: {
+        110000: '北京市',
+        120000: '天津市'
+      },
+      city_list: {
+        110100: '北京市',
+        110200: '县',
+        120100: '天津市',
+        120200: '县'
+      },
+      county_list: {
+        110101: '东城区',
+        110102: '西城区',
+        110105: '朝阳区',
+        110106: '丰台区',
+        120101: '和平区',
+        120102: '河东区',
+        120103: '河西区',
+        120104: '南开区',
+        120105: '河北区',
+      }
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log('areaData', areaData);
 
   },
-
+  //显示城市选择框
+  onShowCity(){
+    this.setData({show: true });
+  },
+  // 关闭城市选择框
+  onCloseCity() {
+    this.setData({ show: false });
+  },
+  // 城市选择框
+  onSelectedCity(e) {
+   console.log("选择的城市",e.detail);
+   this.onCloseCity()
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
